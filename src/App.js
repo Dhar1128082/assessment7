dfbf89afe6df5f4b0963c07cf76a6c2bfaa57ea5
dhar1128082 <dhravya31099194@gmail.com>
2022-11-08 16:49:33 +0530

    show done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
 import './App.css';
-import { BrowserRouter, Routes,Route} from 'react-router-dom';
-import { Home } from './Pages/Home';
-import { Students } from './Pages/Students';
-import { Contact } from './Pages/Contact';
-import { StudentContextProvider } from './StudentContext';
+
+import React from 'react';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Home from "./Components/Home";
+import Students from './Components/Students';
+import ContactUs from './Components/Contactus';
+import Navbar from './Components/Navbar';
+import Addstudent from "./Components/Addstudent"
+import Edit from './Components/Edit';
+import store from './Components/pages/Store'
+import {Provider} from "react-redux";
 
 
-function App() {
-  return (
-    <StudentContextProvider>
-    <BrowserRouter>
-    <Routes>
-     <Route path={'/'} element={<Home/>} />
-      <Route path={'/Pages/Students'} element={<Students/>} />
-      <Route path={'/Pages/Contact'} element={<Contact/>} />
-    </Routes>
-    </BrowserRouter>
-    </StudentContextProvider>
-  );
+
+const App = () => {
+
+  
+    return (
+      
+        <BrowserRouter>
+          <Navbar></Navbar>
+          <div className="App">
+          <Provider store={store}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/students" element={<Students/>} />
+            <Route path="/contact" element={<ContactUs />} />
+            <Route path="/students/add" element={<Addstudent />}/>
+            <Route path="students/edit" element={<Edit />} />
+          </Routes>
+          </Provider>
+          </div>
+        </BrowserRouter>
+    )
+
+  
 }
 
 export default App;
--- src/Pages/reducer.jsx
+++
@@ -1,9 +0,0 @@
-const Reducer = (state="",action) =>{
-    switch(action){
-     case NewStudents : return <NewStudents/>
-     case StudentEdit : return <StudentEdit/>
-     default : return <Students/>
-    }
-}
-
-export default Reducer;
